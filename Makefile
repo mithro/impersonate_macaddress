9b50247f452b2629d259adf70a6c3eabe4b2f2d8
Tim 'mithro' Ansell <mithro@mithis.com>
2017-03-12 14:06:35 +1100

    Adding interface name too.

--- Makefile
+++ Makefile
@@ -2,15 +2,15 @@ LDFLAGS ?= -ldl
 RM ?= rm -f
 CFLAGS ?= -Wall -Wformat-security -fstack-protector --param=ssp-buffer-size=4 -D_FORTIFY_SOURCE=2 -O2 
 
-all: impersonate_macaddress.so  impersonate_macaddress32.so
+all: lib64impersonate_macaddress.so  libimpersonate_macaddress.so
 
-impersonate_macaddress.so: impersonate_macaddress.o
-	$(CC) -shared -o $@ $^ $(LDFLAGS)
+lib64impersonate_macaddress.so: impersonate_macaddress64.o
+	$(CC) -m64 -shared -o $@ $^ $(LDFLAGS)
 
-impersonate_macaddress.o: impersonate_macaddress.c
-	$(CC) -fPIC $(CFLAGS) -o $@ -c $^
+impersonate_macaddress64.o: impersonate_macaddress.c
+	$(CC) -m64 -fPIC $(CFLAGS) -o $@ -c $^
 
-impersonate_macaddress32.so: impersonate_macaddress32.o
+libimpersonate_macaddress.so: impersonate_macaddress32.o
 	$(CC) -m32 -shared -o $@ $^ $(LDFLAGS)
 
 impersonate_macaddress32.o: impersonate_macaddress.c
--- impersonate_macaddress.c
+++ impersonate_macaddress.c
@@ -4,6 +4,7 @@
 
 #define _GNU_SOURCE
 
+#include <errno.h>
 #include <net/if.h>
 #include <net/if_arp.h>
 #include <sys/ioctl.h>
@@ -30,7 +31,31 @@ int ioctl(int d, unsigned long request, ...)
            &mac_addr_buff[1], &mac_addr_buff[2], &mac_addr_buff[3],
            &mac_addr_buff[4], &mac_addr_buff[5]);
 
-	if(request == SIOCGIFHWADDR) {
+	//ioctl(3, SIOCGIFNAME, {ifr_index=0, ifr_name=???}) = -1 ENODEV (No such device)
+	//ioctl(3, SIOCGIFNAME, {ifr_index=1, ifr_name="lo"}) = 0
+	//ioctl(3, SIOCGIFHWADDR, {ifr_name="lo", ifr_hwaddr=00:00:00:00:00:00}) = 0
+	//ioctl(3, SIOCGIFNAME, {ifr_index=2, ifr_name="em0"}) = 0
+	//ioctl(3, SIOCGIFHWADDR, {ifr_name="em0", ifr_hwaddr=aa:bb:cc:dd:ee:ff}) = 0
+	//ioctl(3, SIOCGIFNAME, {ifr_index=3, ifr_name="em1"}) = 0
+	//ioctl(3, SIOCGIFHWADDR, {ifr_name="em1", ifr_hwaddr=aa:bb:cc:dd:ee:f1}) = 0
+
+	if(request == SIOCGIFNAME) {
+		ifr = va_arg(myargs, struct ifreq *);
+
+		if (ifr->ifr_ifindex == 0) {
+			ifr->ifr_name[0] = 'e';
+			ifr->ifr_name[1] = 't';
+			ifr->ifr_name[2] = 'h';
+			ifr->ifr_name[3] = '0';
+			ifr->ifr_name[4] = '\0';
+			ret = 0;
+		} else {
+			errno = ENODEV;
+			ret = -1;
+		}
+		va_end(myargs);
+		return ret;
+	} else if(request == SIOCGIFHWADDR) {
 		ifr = va_arg(myargs, struct ifreq *);
 		ifr->ifr_hwaddr.sa_family = ARPHRD_ETHER;
 		for(i = 0; i < ETHER_ADDR_LEN; i++)
