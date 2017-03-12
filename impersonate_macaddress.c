9d1217861cb522a92fa496edf44250a5a3402e5d
Tim 'mithro' Ansell <mithro@mithis.com>
2017-03-12 15:16:33 +1100

    Don't cause segfaults if MACADDR not set.

--- impersonate_macaddress.c
+++ impersonate_macaddress.c
@@ -28,48 +28,49 @@ int ioctl(int d, unsigned long request, ...)
 
 	va_start(myargs, request);
 	mac_addr = getenv("MACADDR");
-	sscanf(mac_addr, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &mac_addr_buff[0],
-           &mac_addr_buff[1], &mac_addr_buff[2], &mac_addr_buff[3],
-           &mac_addr_buff[4], &mac_addr_buff[5]);
+	if (mac_addr != NULL) {
+		sscanf(mac_addr, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &mac_addr_buff[0],
+		   &mac_addr_buff[1], &mac_addr_buff[2], &mac_addr_buff[3],
+		   &mac_addr_buff[4], &mac_addr_buff[5]);
 
-	//ioctl(3, SIOCGIFNAME, {ifr_index=0, ifr_name=???}) = -1 ENODEV (No such device)
-	//ioctl(3, SIOCGIFNAME, {ifr_index=1, ifr_name="lo"}) = 0
-	//ioctl(3, SIOCGIFHWADDR, {ifr_name="lo", ifr_hwaddr=00:00:00:00:00:00}) = 0
-	//ioctl(3, SIOCGIFNAME, {ifr_index=2, ifr_name="em0"}) = 0
-	//ioctl(3, SIOCGIFHWADDR, {ifr_name="em0", ifr_hwaddr=aa:bb:cc:dd:ee:ff}) = 0
-	//ioctl(3, SIOCGIFNAME, {ifr_index=3, ifr_name="em1"}) = 0
-	//ioctl(3, SIOCGIFHWADDR, {ifr_name="em1", ifr_hwaddr=aa:bb:cc:dd:ee:f1}) = 0
+		//ioctl(3, SIOCGIFNAME, {ifr_index=0, ifr_name=???}) = -1 ENODEV (No such device)
+		//ioctl(3, SIOCGIFNAME, {ifr_index=1, ifr_name="lo"}) = 0
+		//ioctl(3, SIOCGIFHWADDR, {ifr_name="lo", ifr_hwaddr=00:00:00:00:00:00}) = 0
+		//ioctl(3, SIOCGIFNAME, {ifr_index=2, ifr_name="em0"}) = 0
+		//ioctl(3, SIOCGIFHWADDR, {ifr_name="em0", ifr_hwaddr=aa:bb:cc:dd:ee:ff}) = 0
+		//ioctl(3, SIOCGIFNAME, {ifr_index=3, ifr_name="em1"}) = 0
+		//ioctl(3, SIOCGIFHWADDR, {ifr_name="em1", ifr_hwaddr=aa:bb:cc:dd:ee:f1}) = 0
 
-	if(request == SIOCGIFNAME) {
-		ifr = va_arg(myargs, struct ifreq *);
+		if(request == SIOCGIFNAME) {
+			ifr = va_arg(myargs, struct ifreq *);
 
-		if (ifr->ifr_ifindex == 0) {
-			ifr->ifr_name[0] = 'e';
-			ifr->ifr_name[1] = 't';
-			ifr->ifr_name[2] = 'h';
-			ifr->ifr_name[3] = '0';
-			ifr->ifr_name[4] = '\0';
-			ret = 0;
-		} else {
-			errno = ENODEV;
-			ret = -1;
+			if (ifr->ifr_ifindex == 0) {
+				ifr->ifr_name[0] = 'e';
+				ifr->ifr_name[1] = 't';
+				ifr->ifr_name[2] = 'h';
+				ifr->ifr_name[3] = '0';
+				ifr->ifr_name[4] = '\0';
+				ret = 0;
+			} else {
+				errno = ENODEV;
+				ret = -1;
+			}
+			va_end(myargs);
+			return ret;
+		} else if(request == SIOCGIFHWADDR) {
+			ifr = va_arg(myargs, struct ifreq *);
+			ifr->ifr_hwaddr.sa_family = ARPHRD_ETHER;
+			for(i = 0; i < ETHER_ADDR_LEN; i++)
+				ifr->ifr_hwaddr.sa_data[i] = mac_addr_buff[i];
+			va_end(myargs);
+			return 0;
 		}
-		va_end(myargs);
-		return ret;
-	} else if(request == SIOCGIFHWADDR) {
-		ifr = va_arg(myargs, struct ifreq *);
-		ifr->ifr_hwaddr.sa_family = ARPHRD_ETHER;
-		for(i = 0; i < ETHER_ADDR_LEN; i++)
-			ifr->ifr_hwaddr.sa_data[i] = mac_addr_buff[i];
-		va_end(myargs);
-		return 0;
-	} else {
-		int (*real_ioctl)(int, unsigned long, ...);
-		real_ioctl = dlsym(RTLD_NEXT, "ioctl");
-		ret = real_ioctl(d, request, myargs);
-		va_end(myargs);
-		return ret;
 	}
+	int (*real_ioctl)(int, unsigned long, ...);
+	real_ioctl = dlsym(RTLD_NEXT, "ioctl");
+	ret = real_ioctl(d, request, myargs);
+	va_end(myargs);
+	return ret;
 }
 
 /*
